4255347942466ab03d13d713f6a57ecf2db06164
Sylvain Desbureaux <sylvain.desbureaux@orange.com>
2020-04-03 11:51:58 +0200

    Allow SDC client to connect to SDC in HTTP
    I've added a toggle which allow to connect to SDC using HTTP and not
    only HTTPS.
    
    By default, we're still on HTTPS.
    
    Issue-ID: SDC-2721
    Signed-off-by: Sylvain Desbureaux <sylvain.desbureaux@orange.com>
    Change-Id: I48046c19aed7d81f9e03a30b3b081b9d8dd1495e

--- sdc-distribution-client/src/main/java/org/onap/sdc/api/consumer/IConfiguration.java
+++ sdc-distribution-client/src/main/java/org/onap/sdc/api/consumer/IConfiguration.java
@@ -45,6 +45,16 @@ public interface IConfiguration {
     String getUser();
 
     /**
+     * Return True if ssl is needed, false otherwise.
+     * This param can be null, then default (HTTPS) behavior will be
+     * applied. If set to false, distribution client will use HTTP when
+     * connecting to SDC.
+     *
+     * @return
+     */
+    Boolean isUseHttpsWithSDC();
+
+    /**
      * User Password for SDC distribution consumer authentication.
      *
      * @return User Password.
